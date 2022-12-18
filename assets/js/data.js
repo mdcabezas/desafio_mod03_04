e8dc14cefe416c94cd181f565c3121f618482439
Manuel Cabezas <manuelcabezas@gmail.com>
2022-12-18 19:11:28 -0400

    feat: add filter

---
+++ assets/js/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    nombre: "Casa de campo",
+    descripcion: "Un lugar ideal para descansar de la ciudad",
+    src: "./assets/img/casa-de-campo.jpg",
+    cuartos: 2,
+    metros: 170
+  },
+  {
+    nombre: "Casa de playa",
+    descripcion: "Despierta tus días oyendo el oceano",
+    src: "./assets/img/casa-en-la-playa.jpg",
+    cuartos: 2,
+    metros: 130
+  },
+  {
+    nombre: "Casa en el centro",
+    descripcion: "Ten cerca de ti todo lo que necesitas",
+    src: "./assets/img/casa-en-el-centro.jpg",
+    cuartos: 1,
+    metros: 80
+  },
+  {
+    nombre: "Casa rodante",
+    descripcion: "Conviertete en un nómada del mundo sin salir de tu casa",
+    src: "./assets/img/casa-rodante.jpg",
+    cuartos: 1,
+    metros: 6
+  },
+  {
+    nombre: "Departamento",
+    descripcion: "Desde las alturas todo se ve mejor",
+    src: "./assets/img/departamento.jpg",
+    cuartos: 3,
+    metros: 200
+  },
+  {
+    nombre: "Mansión",
+    descripcion: "Vive una vida lujosa en la mansión de tus sueños ",
+    src: "./assets/img/mansion.jpg",
+    cuartos: 5,
+    metros: 500
+  }
+];
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,51 +1,86 @@
-const propiedadesJSON = [
-    {
-      name: "Casa de campo",
-      description: "Un lugar ideal para descansar de la ciudad",
-      src:
-        "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
-      rooms: 2,
-      m: 170
-    },
-    {
-      name: "Casa de playa",
-      description: "Despierta tus días oyendo el oceano",
-      src:
-        "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
-      rooms: 2,
-      m: 130
-    },
-    {
-      name: "Casa en el centro",
-      description: "Ten cerca de ti todo lo que necesitas",
-      src:
-        "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-      rooms: 1,
-      m: 80
-    },
-    {
-      name: "Casa rodante",
-      description: "Conviertete en un nómada del mundo sin salir de tu casa",
-      src:
-        "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
-      rooms: 1,
-      m: 6
-    },
-    {
-      name: "Departamento",
-      description: "Desde las alturas todo se ve mejor",
-      src:
-        "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
-      rooms: 3,
-      m: 200
-    },
-    {
-      name: "Mansión",
-      description: "Vive una vida lujosa en la mansión de tus sueños ",
-      src:
-        "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
-      rooms: 5,
-      m: 500
+// Inicializar variables de DOM
+const $room = document.querySelector("#room");
+const $from = document.querySelector("#from");
+const $to = document.querySelector("#to");
+const $total = document.querySelector("#total");
+const $realstateproperty = document.querySelector("#realstateproperty");
+
+const search = () => {
+
+  //Inicializar valores para operar
+  let result = "";
+  let count = 0;
+  let room = Number($room.value);
+  let from = Number($from.value);
+  let to = Number($to.value);
+
+  // Descartar campos vacios en formulario
+  if (!room || !from || !to) {
+    return alert("Faltan campos por llenar")
+  }
+
+  // Iterar arreglo de objetos en ./assets/js/data.js
+  for (let item of data) {
+
+    if (room === item.cuartos && from <= item.metros && to >= item.metros) {
+
+      // Concatenar nuevo elemento
+      result += ` 
+                <div class="propiedad">
+                  <div class="img" style="background-image: url('${item.src}')"></div>
+                  <section>
+                    <h5>${item.nombre}</h5>
+                      <div class="d-flex justify-content-between">
+                        <p>Cuartos: ${item.cuartos}</p>
+                        <p>Metros: ${item.metros}</p>
+                      </div>
+                    <p class="my-3">${item.descripcion}</p>
+                    <button class="btn btn-info ">Ver más</button>
+                  </section>
+                </div>
+                `
+      // Sumar al total
+      count++
+
     }
-  ];
-  
+
+    // Insertar valores procesados en HTML
+    $realstateproperty.innerHTML = result
+    $total.innerHTML = count
+
+  }
+}
+
+// Cargar todos los items al iniciar <body onload="allItems()">
+const allItems = () => {
+
+  //Inicializar valores para operar
+  let result = ""
+  let count = 0;
+
+  // Iterar arreglo de objetos en ./assets/js/data.js
+  for (let item of data) {
+
+    // Concatenar nuevo elemento
+    result += ` 
+                <div class="propiedad">
+                  <div class="img" style="background-image: url('${item.src}')"></div>
+                  <section>
+                    <h5>${item.nombre}</h5>
+                      <div class="d-flex justify-content-between">
+                        <p>Cuartos: ${item.cuartos}</p>
+                        <p>Metros: ${item.metros}</p>
+                      </div>
+                    <p class="my-3">${item.descripcion}</p>
+                    <button class="btn btn-info ">Ver más</button>
+                  </section>
+                </div>
+                `
+    // Sumar al total
+    count++
+  }
+  // Insertar valores procesados en HTML
+  $realstateproperty.innerHTML = result
+  $total.innerHTML = count
+
+}
